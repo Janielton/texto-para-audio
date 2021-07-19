73a5911465f83a6e63f1febed206c9e408df847d
Janielton <janieltoncnn@gmail.com>
2021-07-19 12:26:38 -0300

    Constant config

---
+++ Classes/Constats.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gerador_Audio.Classes
+{
+    public class Constats
+    {
+        public static readonly string apyKey = "{key do serviço}";
+        public static readonly string apiUrl = "https://api.us-east.text-to-speech.watson.cloud.ibm.com";
+    }
+}
